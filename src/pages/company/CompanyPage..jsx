023f15ec14211cc706df49429069208f4eba96bc
mfukechristian <mfukechristian@gmail.com>
2025-04-21 14:03:14 +0200

    feat: add industry filter functionality to CompanyPage with styling

--- src/pages/company/CompanyPage..jsx
+++ src/pages/company/CompanyPage..jsx
@@ -1,17 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AiOutlineClose } from "react-icons/ai";
 import { fetchAllCompanies } from "../../../redux/actions/companyAction";
 import { Link } from "react-router-dom";
 import "./CompanyPage.css";
 
 const CompanyPage = () => {
   const dispatch = useDispatch();
+  const [activeFilter, setActiveFilter] = useState(null);
 
   const { companies, loading, error } = useSelector((state) => state.companies);
 
   useEffect(() => {
     dispatch(fetchAllCompanies());
   }, [dispatch]);
+
+  const handleFilterClick = (industry) => {
+    setActiveFilter((prev) => (prev === industry ? null : industry));
+  };
+
+  const filteredCompanies = activeFilter
+    ? companies.filter((company) => company.industry === activeFilter)
+    : companies;
+
+  const industries = ["Fintech", "Marketplace"];
 
   return (
     <div className="company-page">
@@ -22,14 +34,30 @@ const CompanyPage = () => {
           shaping the future. Explore their innovative products and the change
           they're creating.
         </p>
+      </div>{" "}
+      <div className="industry-filters">
+        {industries.map((industry) => (
+          <button
+            key={industry}
+            className={`filter-button ${
+              activeFilter === industry ? "active" : ""
+            }`}
+            onClick={() => handleFilterClick(industry)}
+          >
+            {industry}
+            {activeFilter === industry && (
+              <span className="remove-icon">
+                <AiOutlineClose />
+              </span>
+            )}
+          </button>
+        ))}
       </div>
-
       {loading && <p>Loading companies...</p>}
       {error && <p>Error: {error}</p>}
-
       <div className="company-cards">
-        {companies?.length > 0
-          ? companies.map((company) => (
+        {filteredCompanies?.length > 0
+          ? filteredCompanies.map((company) => (
               <Link
                 to={`/detail/${company._id}`}
                 className="company-card-link"
